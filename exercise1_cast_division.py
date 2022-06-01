d5bdcbecc894dd048196e66e78a54973720fc0aa
Milton Nabor <65313450+ToneX1090@users.noreply.github.com>
2022-05-31 22:00:39 -0600

    Add files via upload
    exercise that defines the type(float) of a variable and does a division calculation.

---
+++ exercise1_cast_division.py
@@ -0,0 +1,9 @@
+def dividir():
+
+    number1 = float(input ("Digite o primeiro numero a ser dividido: " ))
+    number2 = float(input ("Digite o segundo numero a ser dividido: " ))
+    
+    division = number1/number2
+
+    print("A divisão de", number1, "por", number2, "é", division)
+dividir()
