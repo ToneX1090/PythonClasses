c701fcd53ed6a7aa90397ee472a8387b2b8439d3
Milton Nabor <65313450+ToneX1090@users.noreply.github.com>
2022-05-31 22:01:48 -0600

    Add files via upload
    exercise that defines the type of a variable and does a four numbers subtraction calculation.

---
+++ exercise2_cast_subtraction.py
@@ -0,0 +1,11 @@
+def sub():
+
+    number1 = float(input ("Digite o primeiro numero a ser subtraido: " ))
+    number2 = float(input ("Digite o segundo numero a ser subtraido: " ))
+    number3 = float(input ("Digite o terceiro numero a ser subtraido: " ))
+    number4 = float(input ("Digite o quarto numero a ser subtraido: " ))
+
+    subtraction = number1 - number2 - number3 - number4
+
+    print(number1, "menos", number2, "menos", number3, "menos", number4, "é igual a: ", subtraction)
+sub()
