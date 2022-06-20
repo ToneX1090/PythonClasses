ff777f137433761f46ef2dd73c5ad9eaab87512c
ToneX1090 <nabor1210@gmail.com>
2022-06-20 12:48:38 -0600

    Learning list and adding a new item to it

---
+++ List_create_n_add.py
@@ -0,0 +1,21 @@
+def main():
+
+    #creating a string list
+    first_list = ["Milton", "Juan", "Rafaela", "Fernando"]
+#positions > ........1º......2º........3º........4º.....
+#python_positions >...0.......1........2..........3.....
+
+#......This line prints "Rafaela"...second python position.
+    print(first_list[2])
+
+#adding a new item using .append
+
+    first_list.append("new student")
+
+    print(first_list)
+
+# it is important to remember that the new student will be added at position 5 of the list and 4 at position python.
+
+
+
+main()
