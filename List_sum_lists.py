7249fde4168500b1d9012262dbc115c8b31d1fc7
ToneX1090 <nabor1210@gmail.com>
2022-07-19 22:18:54 -0600

    Sum lists

---
+++ List_sum_lists.py
@@ -0,0 +1,9 @@
+students_lv1 = ["Pedro" , "Luiza", "Murilo"]
+students_lv2 = ["Matheus", "Clara", "Manu"]
+
+print ("The students of level one are: {}" .format(students_lv1))
+print ("The students of level one are: {}" .format(students_lv2))
+
+all_students = students_lv1 + students_lv2
+
+print("{} are all our students".format(all_students))
