1acce2dac01784a670756ba5422f41bc7bcdfc57
ToneX1090 <nabor1210@gmail.com>
2022-08-01 23:16:06 -0600

    Study of lists native functions of Python

--- List_native_functions.py
+++ List_native_functions.py
@@ -1,4 +1,4 @@
-#first function => append() Insert a element in the final of the list
+#First function: append() => Insert a element in the final of the list
 
 numbers = ["one"]
 print(numbers)
@@ -9,7 +9,8 @@ print(numbers)
 
 print("\n")
 
-#Second function Index() => Return a Index of one determined element
+   #Second function: Index() => Return a Index of one determined element
+
 lenguages = ["Python", "Java", "C+", "VB"]
 # Indexes ------0---------1-----2-----3---
 
@@ -22,7 +23,50 @@ print(lenguages.index("Python"))
 
 print("\n")
 
-#Seventh fuction => Sort the numbers of the list
+   #Third function: insert() => insert an element in a list in a determinated position
+
+animals = ["Cat", "Dog", "Duck"]
+#index-------0------1-------2---
+print (animals)
+
+#add Ferret in posintion 1
+animals.insert(1,"Ferret")
+
+print(animals)
+
+print("\n")
+
+    #Fourth function: remove => Remove a determined object
+
+animals.remove("Dog")
+
+print(animals)
+
+print("\n")
+
+    #Fifth function: pop => Remove an element in the determined index
+
+lenguages = ["Python", "Java", "C+", "VB"]
+# Indexes ------0---------1-----2-----3---
+
+lenguages.pop(3)
+print(lenguages)
+
+# !!! CAUTION with the limit of indexes, is no possible to remove a inexisted index(item)
+
+print("\n")
+
+    #Sixth fuction: del => Remove a element in a determinated index pass by parameter(index).
+
+lenguages = ["Python", "Java", "C+", "VB"]
+# Indexes ------0---------1-----2-----3---
+
+del(lenguages[2])
+print(lenguages)
+
+print("\n")
+
+   #Seventh fuction: Sort => the numbers of the list
 
 list = [2, 4, 1]
 print(list)
