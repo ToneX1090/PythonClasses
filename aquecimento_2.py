659ccb48c07ca5ee8ef33b7830eba62e6a61b43e
ToneX1090 <nabor1210@gmail.com>
2024-01-23 20:13:41 -0600

    in progress

--- aquecimento_2.py
+++ aquecimento_2.py
@@ -1,25 +1,30 @@
 import sys
 
 #win_game = input (str("Digite o jogo vencedor: "))
-win_game = ["03 22 10 12 43 38"] #esta pegando o /n
+win_game = "03 22 10 12 43 38" #esta pegando o /n
 #cont = 0
 int_games = []
-int_number = []
-lista = [[1, 2, 3, 5, 4], [1, 5, 8, 6, 2], [8, 6, 9, 5, 2]]
+#int_number = []
+#lista = [[1, 2, 3, 5, 4], [1, 5, 8, 6, 2], [8, 6, 9, 5, 2]]
 
 archive = open(sys.argv[1], "r", encoding='utf-8')
 game_list = archive.readlines()
+win_numbers = set(win_game.split(" "))
 
-for games in game_list:
+
+for game in game_list:
+
+    numbers = game.strip().split(" ")
+    correct_numbers = 0
+    for number in numbers:
+        if number in win_numbers:
+            correct_numbers += 1
     
-    for game in games.split():
-        int_number.append(int(game))
-    int_games.append(int_number)
+    print(game + " - " + str(correct_numbers) + " corretos")
 
-print(lista)
-print(type(lista))
-print(int_games)
 
+   
+    print()
 
 
 
