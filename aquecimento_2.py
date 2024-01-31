af41585d989f364ebbcfea38447b8d697ea38920
ToneX1090 <nabor1210@gmail.com>
2024-01-30 21:21:02 -0600

    Receives a lottery game and compares it to a txt file with your played games

--- aquecimento_2.py
+++ aquecimento_2.py
@@ -1,8 +1,6 @@
 import sys
 
-#win_game = input (str("Digite o jogo vencedor: "))
-win_game = "03 22 10 12 44 38" #esta pegando o /n
-#cont = 0
+win_game = input (str("Digite o jogo vencedor: "))
 
 archive = open(sys.argv[1], "r", encoding='utf-8')
 game_list = archive.readlines()
@@ -16,21 +14,14 @@ for game in game_list:
     for number in numbers:
         if number in win_numbers:
             correct_numbers += 1
-    
-   
-    
-    print(game + " - " + str(correct_numbers) + " corretos")
-            
-    """ if correct_numbers == 4:
-        print(game + " - É uma quadra.")
+    if correct_numbers == 4:
+        print(game + "\nÉ uma quadra.\n")
     elif correct_numbers == 5:
-        print(game + " - É uma quina!")
+        print(game + "\nÉ uma quina!\n")
     elif correct_numbers == 6:
-        print(" Larga tudo que você está rico!")"""
-
-
-
-
-
+        print("\nLarga tudo que você está rico!\n")
+    else:
+        print(game + "\n" + str(correct_numbers) + " corretos\n")
+            
 
 # "C:\Users\Milton\Documents\mega.txt"
